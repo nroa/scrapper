08be0d6492f1d440b7e72ad24242711d5568affd
Norman Roa <norman@thecoursekey.com>
2020-11-22 22:18:26 -0700

    Fetching a page

--- handler.js
+++ handler.js
@@ -2,25 +2,28 @@
 
 const { getPage, parsePage, saveRatingsToDB } = require("./utils")
 
-module.exports.hello = async event => {
+module.exports.scrape = async event => {
 
   // 1. fetch yelp page
+
+  getPage(event).then(page => console.log(page));
+
 
   // 2. parse the page
 
   // 3. save ratings data to our db
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  // return {
+  //   statusCode: 200,
+  //   body: JSON.stringify(
+  //     {
+  //       message: 'Go Serverless v1.0! Your function executed successfully!',
+  //       input: event,
+  //     },
+  //     null,
+  //     2
+  //   ),
+  // };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
--- utils/getPage.js
+++ utils/getPage.js
@@ -1,3 +1,11 @@
-module.exports = () => {
-    // 
+const request = require('request-promise');
+
+module.exports = businessName => {
+  // https://www.yelp.com/biz/the-last-bookstore-los-angeles
+  
+  const url = `https://www.yelp.com/biz/${businessName}`;
+
+  return request({ method: 'GET', url: url });
+
+
 }
