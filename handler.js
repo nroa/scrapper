a91b19d41ddd9672777ecfffb1993c31a4ed1ffa
Norman Roa <norman@thecoursekey.com>
2020-11-22 21:49:16 -0700

    first commit

---
+++ handler.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { getPage, parsePage, saveRatingsToDB } = require("./utils")
+
+module.exports.hello = async event => {
+
+  // 1. fetch yelp page
+
+  // 2. parse the page
+
+  // 3. save ratings data to our db
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
